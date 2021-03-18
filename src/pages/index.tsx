dba6e20377774c44ed28de8c47c6a72c56bc38e5
Vinci Taylaran <svl.taylaran@gmail.com>
2021-03-17 20:21:20 -0600

    initial files

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import * as React from "react"
+import * as styles from "./index.module.scss"
+
+// markup
+const IndexPage = () => {
+  return (
+    <main className={styles.content}>
+      <title>Home Page</title>
+      <h1>Hello World</h1>
+    </main>
+  )
+}
+
+export default IndexPage
