6ea8cffc134e3674fadcc9e75749b94dba9f1b56
Vinci Taylaran <svl.taylaran@gmail.com>
2021-03-17 20:34:22 -0600

    feat: reset CSS. create variables, import font family

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,11 @@
 import * as React from "react"
 import * as styles from "./index.module.scss"
+import "../styles/globals.scss"
 
-// markup
 const IndexPage = () => {
   return (
     <main className={styles.content}>
-      <title>Home Page</title>
+      <title>crowdfund</title>
       <h1>Hello World</h1>
     </main>
   )
