0ae71b6d8ef856f3ff4ad6a696ce1479f6d6adf4
Vinci Taylaran <svl.taylaran@gmail.com>
2021-03-17 22:37:26 -0600

    feat: install plugins that convert .svg files to React components, install plugin for breakpoints

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,28 @@
+const myCustomQueries = {
+  xs: `(max-width: 425px)`,
+}
+
 module.exports = {
   siteMetadata: {
     title: "crowdfunding-product-page",
   },
-  plugins: ["gatsby-plugin-sass"],
-};
+  plugins: [
+    `gatsby-plugin-sass`,
+    `gatsby-plugin-react-svg`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-breakpoints`,
+      options: {
+        queries: myCustomQueries,
+      },
+    },
+  ],
+}
