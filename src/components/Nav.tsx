d87ac8948e387c59d35fb797ecc9d9fe29cd6640
Vinci Taylaran <svl.taylaran@gmail.com>
2021-03-18 10:10:07 -0600

    feat: hero has `z-index: -2`, background for navbar has `z-index: -1`

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -4,22 +4,28 @@ import * as styles from "./nav.module.scss"
 import HamburgerIcon from "../images/icon-hamburger.svg"
 import Logo from "../images/logo.svg"
 
+const LinearGradientBg = () => {
+  return <div className={styles.linearGradientBg}></div>
+}
+
 const Nav = () => {
-  // TODO: fix issue where the navbar children are losing opacity. Children should have an opacity of 1, but their container should be 0.6.
   return (
-    <nav className={styles.navbar}>
-      <Logo />
+    <>
+      <nav className={styles.navbar}>
+        <Logo />
 
-      <div className={styles.hamburgerContainer}>
-        <HamburgerIcon />
-      </div>
+        <div className={styles.hamburgerContainer}>
+          <HamburgerIcon />
+        </div>
 
-      <div className={styles.links}>
-        <a href="#">About</a>
-        <a href="#">Discover</a>
-        <a href="#">Get Started</a>
-      </div>
-    </nav>
+        <div className={styles.links}>
+          <a href="#">About</a>
+          <a href="#">Discover</a>
+          <a href="#">Get Started</a>
+        </div>
+      </nav>
+      <LinearGradientBg />
+    </>
   )
 }
 
